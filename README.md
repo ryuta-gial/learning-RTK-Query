1163d706a0aef2d3ee38b8662c17b124a054fcfb
ryuta-gial <shimizuwork000@gmail.com>
2022-12-06 18:08:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# learning-redux-toolkit-query
+
+## Overview
+
+- learning RTK Query
+
+## Usage
+
+
+```
+npm i json-server -g
+
+json-server --watch src/data/db.json --port 3500 
+
+npm install
+
+npm run start
+```
---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,53 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+type TodosResponse = Todo[];
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  tagTypes: ['Todos'],
+  endpoints: (builder) => ({
+    getTodos: builder.query<TodosResponse,void>({
+      query: () => '/todos',
+      transformResponse: (res:Todo[]) => res.sort((a, b) => b.id - a.id),
+      providesTags: ['Todos'],
+    }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: '/todos',
+        method: 'POST',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: 'PATCH',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: 'DELETE',
+        body: id,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+  }),
+});
+
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
